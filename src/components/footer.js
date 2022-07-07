4e85585d094ca0702988ef7e578a864e9c32aa76
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2022-07-07 17:57:59 +0200

    cms Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {useStyle} from './context/styleContext'
-
+import { graphql, useStaticQuery } from "gatsby";
+import { useIntl } from "gatsby-plugin-intl";
+import { useStyle } from "./context/styleContext";
 import logo from "../images/logo_footer.png";
 import logoMobile from "../images/logo_footer__mobile.png";
 import fb from "../images/footer/fb.png";
@@ -10,42 +11,94 @@ import snapchat from "../images/footer/snapchat.png";
 import { Link } from "gatsby";
 
 const Footer = () => {
-  const style = useStyle()
+  const style = useStyle();
+  const intl = useIntl();
+  const data = useStaticQuery(graphql`
+    {
+      allWpFooterItems {
+        edges {
+          node {
+            language {
+              slug
+            }
+            infoColumn {
+              type
+            }
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  let columnList = [];
+  const items = data.allWpFooterItems.edges.filter((n) => {
+    return n.node.language.slug === intl.locale;
+  });
+  items.map((i) => {
+    columnList.push(i.node.infoColumn.type);
+  });
+
+  columnList = [...new Set(columnList)];
   const soMe = (img) => {
     return (
-      <Link to="/" className="w-12 mr-5 md:mr-auto md:ml-5 ">
+      <Link to="/" className="w-12 mr-5 md:mr-auto md:ml-5">
         <img src={img} alt="logo" className="w-full " />
       </Link>
     );
   };
 
-  const soMeList = [fb,ig,twitter,snapchat]
-  const h4= ` ${style.mt_sm}  mb-3`
+  const soMeList = [fb, ig, twitter, snapchat];
+  const h4 = ` ${style.mt_sm}  mb-3`;
   return (
     <section className={`footer ${style.mx}  ${style.my} `}>
+      {console.log("columnList", columnList)}
       <div className="top w-full md:flex justify-between md:mb-10 md:mt-14">
         <div className="w-full md:w-28 ">
           <Link to="/" className="w-full ">
             <img src={logo} alt="logo" className="w-full hidden md:block" />
-            <img
-              src={logoMobile}
-              alt="logo"
-              className="w-full md:hidden "
-            />
+            <img src={logoMobile} alt="logo" className="w-full md:hidden " />
           </Link>
         </div>
         <div className={`my-8 follow md:flex items-center md:my-auto`}>
-          <h4 className={`${style.text.h4} my-6 md:mr-10`}>
-            {" "}
-            Follow us on
-          </h4>
+          <h4 className={`${style.text.h4} my-6 md:mr-10`}> Follow us on</h4>
           <div className="some flex /md:justify-end">
-          {soMeList.map(s=>soMe(s))}
+            {soMeList.map((s) => soMe(s))}
           </div>
         </div>
-        </div>
-        <div className={`info grid grid-rows-2 md:grid-rows-1 grid-cols-2 md:grid-cols-4 ${style.text.action2}`}>
-          <div>
+      </div>
+      <div
+        className={`info grid grid-rows-2 md:grid-rows-1 grid-cols-2 md:grid-cols-4 ${style.text.action2}`}
+      >
+        {columnList.map((c) => {
+          return (
+            <div>
+              <h4 className={`${style.text.h4} ${h4}`}>{c}</h4>
+              <ul>
+                {items.filter((i)=>{
+                  return i.node.infoColumn.type === c
+                })
+                
+                .map((i) => {
+                  return <li>{i.node.title}</li>;
+                })}
+                {/* <li>
+              Info item
+              </li>
+              <li>
+              Info item
+              </li>
+              <li>
+              Info item
+              </li>
+              <li>
+              Info item
+              </li> */}
+              </ul>
+            </div>
+          );
+        })}
+        {/* <div>
             <h4 className={`${style.text.h4} ${h4}`}>Info column</h4>
             <ul>
               <li>
@@ -61,8 +114,8 @@ const Footer = () => {
               Info item
               </li>
             </ul>
-          </div>
-          <div>
+          </div> */}
+        {/* <div>
           <h4 className={`${style.text.h4} ${h4}`}>Info column</h4>
             <ul>
               <li>
@@ -112,12 +165,11 @@ const Footer = () => {
               Info item
               </li>
             </ul>
-          </div>
-
-        </div>
-        <p className={` ${style.text.action2} ${style.mt_md} `}>
+          </div> */}
+      </div>
+      <p className={` ${style.text.action2} ${style.mt_md} `}>
         © 2022 All Rights Reserved.
-          </p>
+      </p>
     </section>
   );
 };
